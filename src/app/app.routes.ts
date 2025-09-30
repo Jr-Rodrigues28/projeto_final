c77b47c008664536fb747e7f4e18763b9fa04bad
Junior Rodrigues <7jsdrcj@gmail.com>
2025-09-30 11:04:37 -0300

    Formatando e estilizando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Contato } from './componentes/contato/contato';
+import { Home } from './componentes/home/home';
+import { Quemsomos } from './componentes/quemsomos/quemsomos';
+import { Cadastro } from './componentes/cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "contato", component: Contato, title: "Contato" },
+  { path: "home", component: Home, title: "Home" },
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "quemsomos", component: Quemsomos, title: "Quem somos" },
+  { path: "cadastro", component: Cadastro, title: "Cadastro" },
+];
