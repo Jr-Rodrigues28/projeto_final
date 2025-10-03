ebafe9ac44c45a708ae9dbd9a8e94a26fccb4b41
Junior Rodrigues <7jsdrcj@gmail.com>
2025-10-03 10:58:57 -0300

    Responsividade

--- src/app/componentes/nav/nav.ts
+++ src/app/componentes/nav/nav.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Nav {
 
+  ativarMenu(e: PointerEvent) {
+    if (e.currentTarget === null)
+      return;
+    const menu = document.getElementById('navbarSupportedContent');
+    console.log(menu);
+    if (menu?.classList.contains("collapse")) {
+      menu.classList.remove("collapse")
+    }
+    else {
+      menu?.classList.add("collapse")
+    }
+  }
 }
